66a5600cb050cc5cfa8ac22f472208b2b1f2623a
fru <florian@rueberg.eu>
2021-12-10 16:05:38 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='aim-remote',
     version='0.0.1',
-    packages=find_packages(include=['aim-remote', 'aim-remote.*']),
+    packages=find_packages(include=['aimremote', 'aimremote.*']),
     install_requires=[
         'aim'
     ]
