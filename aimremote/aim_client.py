b4c74411c64a786ae01e00172d231f45909bce0b
fru <florian@rueberg.eu>
2021-12-11 14:20:11 +0000

    Implement client

--- aimremote/aim_client.py
+++ aimremote/aim_client.py
@@ -1,11 +1,32 @@
-from aim import Run as AimRun
+import pickle
 
 class Run:
-    def test(self):
-        my_run = AimRun(
-            repo='/home/administrator/tools',
-            experiment='experiment_fru2',
-            run_hash='testhash3',
-            system_tracking_interval = None
-        )
-        print("test")
+    def __init__(self, **kwargs):
+        self.url = kwargs['url']
+        self.params = {
+            'repo': kwargs['repo'],
+            'experiment': kwargs['experiment'],
+            'run_hash': kwargs['run_hash'],
+            'security_token': kwargs['security_token']
+        }
+
+    def __getitem__(self, key):
+        print(key)
+        url = self.url + '/aimremote/get_values/' + key
+        return requests.get(url, params=self.params).json()
+
+    def __setitem__(self, key, value):
+        print(key, value)
+        url = self.url + '/aimremote/set_values/' + key
+        requests.post(url, params=self.params, json=value)
+
+    def track(self, data, **kwargs):
+        print(data, kwargs)
+        url = self.url + '/aimremote/track/'
+        encoded = pickle.dumps([
+            { 
+                'data': data,
+                'kwargs': kwargs
+            }
+        ])
+        requests.post(url, params=self.params, data=encoded)
--- aimremote/aim_server.py
+++ aimremote/aim_server.py
@@ -8,10 +8,10 @@ app = FastAPI()
 run_cache = dict()
 global_security_token = ''
 
-def get_run(experiment: str, run_hash: str):
+def get_run(experiment: str, repo: str, run_hash: str):
     if not run_hash in run_cache:
         run_cache[run_hash] = AimRun(
-            repo='/home/administrator/tools',
+            repo=repo,
             experiment=experiment,
             run_hash=run_hash,
             system_tracking_interval = None
@@ -19,33 +19,26 @@ def get_run(experiment: str, run_hash: str):
     return run_cache[run_hash]
 
 @app.get("/aimremote/get_values/{name}")
-async def get_values(name: str, run_hash: str, security_token: str, experiment: str = ''):
+async def get_values(name: str, repo: str, run_hash: str, security_token: str, experiment: str = ''):
     if security_token != global_security_token:
         raise Exception('Wrong token')
-    return get_run(experiment, run_hash)[name]
+    return get_run(experiment, repo, run_hash)[name]
 
 @app.post("/aimremote/set_values/{name}")
-async def get_values(request: Request, name: str, run_hash: str, security_token: str, experiment: str = ''):
+async def get_values(request: Request, name: str, repo: str, run_hash: str, security_token: str, experiment: str = ''):
     if security_token != global_security_token:
         raise Exception('Wrong token')
-    get_run(experiment, run_hash)[name] = await request.json()
+    get_run(experiment, repo, run_hash)[name] = await request.json()
 
 @app.post("/aimremote/track")
-async def track(request: Request, run_hash: str, security_token: str, experiment: str = ''):
+async def track(request: Request, repo: str, run_hash: str, security_token: str, experiment: str = ''):
     if security_token != global_security_token:
         raise Exception('Wrong token')
     data: bytes = await request.body()
     items = pickle.loads(data)
-    run = get_run(experiment, run_hash)
+    run = get_run(experiment, repo, run_hash)
     for item in items:
-        run.track(
-            item['data'],
-            name=item['name'],
-            step=item['step'],
-            epoch=item['epoch'],
-            context=item['context']
-        )
-
+        run.track(item['data'], item['kwargs'])
 
 async def up(host, port, security_token):
     global global_security_token
